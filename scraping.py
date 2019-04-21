24570e209348f0e08ff4899e325a453cf17acad1
ramo <knht31i@gmail.com>
2019-04-21 21:59:35 +0900

    若干速度が遅い気がする（ターミナルに表示される速度）が、elementを用いて取得するように変更した。綺麗に整列しているのでxpathで検索している。

--- scraping.py
+++ scraping.py
@@ -14,42 +14,24 @@ sleep(2)
 
 driver.find_element_by_xpath('//*[@id="link"]').click()
 
-# test = driver.find_elements_by_class_name('library-info-data')
-
-# memo:elementsはstrで返ってくる
-# print(test[1].text)
-# print(" ")
-# print(test[2].text)
-# print(" ")
-# print(test[3].text)
-
-# memo:strで返ってきて改行も含まれる。ターミナルでは改行して出てくるけど実際は一つの変数に入っている
-# a = test[1].text
-# for b in a:
-#     print(b)
-
-# strの選択範囲で表示する使用がよくわからない。改行が混じってるのかもしれない？
-# tmp = test[2].text
-# print(tmp)
-# print("")
-# print(tmp[0])
-# print("")
-# print(tmp[2:4])
 
 
-rank = 1
-for tmp in test:
-    tmp = tmp.text
-    if rank <= 10:
-        num = tmp[2:4]
-    else:
-        num = tmp[3:5]
-    
-    
-    print(str(rank) + "位" + str(num) + "冊")
-    
+for no in range(2,501):
+    no = str(no)
+    rank_path = '//*[@id="srv_bestreading_index"]/table/tbody/tr[' + no + ']/td[1]'
+    rank = driver.find_element_by_xpath(rank_path).text #順位
 
-    rank = rank+1
+    rank_path = '//*[@id="srv_bestreading_index"]/table/tbody/tr[' + no + ']/td[2]'
+    num = driver.find_element_by_xpath(rank_path).text #冊数
+
+    rank_path = '//*[@id="srv_bestreading_index"]/table/tbody/tr[' + no + ']/td[4]/a'
+    title = driver.find_element_by_xpath(rank_path).text #タイトル
+
+    print(rank)
+    print(num)
+    print(title)
+
+
 
 sleep(5)
 driver.quit()
